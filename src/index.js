daf1508f0551ca3926c8978b7a1408c5795a2a91
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-07 12:35:39 +0200

    via reduce and "best results" map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getBestRacer(distance) {
-  // your solution here
+    return distance.reduce(
+        getTrackBestResult,
+        {
+            frogIndex: 0,
+            jumpsCount: Infinity,
+        }
+    ).frogIndex;
+};
+
+function getTrackBestResult(bestResult, currentFrogTrack, currentFrogIndex) {
+    let jumpsCount = 0;
+    let currIndexOnTrack = 0;
+
+    while (currIndexOnTrack < currentFrogTrack.length) {
+        jumpsCount++;
+        currIndexOnTrack += currentFrogTrack[currIndexOnTrack];
+    }
+
+    if (jumpsCount < bestResult.jumpsCount && currentFrogIndex !== bestResult.frogIndex) {
+        bestResult.jumpsCount = jumpsCount;
+        bestResult.frogIndex = currentFrogIndex;
+    }
+
+    return bestResult;
 }
